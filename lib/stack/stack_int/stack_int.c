6f1389a628df5218305f94c806c27c590e8b71fd
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2026-05-08 20:49:32 +0530

    Refactor stack_int

--- lib/stack/stack_int/stack_int.c
+++ lib/stack/stack_int/stack_int.c
@@ -1,78 +1,89 @@
-#include <errno.h>
-#include <stdlib.h>
 #include "stack_int.h"
 #include "common_macros.h"
-
-struct IntegerStackNode;
-typedef struct IntegerStackNode *IntStackNode;
+#include <errno.h>
+#include <stdlib.h>
+#include <memory.h>
 
 #define STACK IntStack
 #define ITEM IntStackItem
-#define NODE IntStackNode
+#define ITER IntStackIterator
+#define NODE struct IntegerStackNode *
 
 struct IntegerStackNode {
+  ITEM item;
   NODE next;
-  ITEM data;
 };
-
-struct IntegerStack {
-  NODE top;
-  size_t size;
-};
-
 
 STACK IntStack_Create() {
-  STACK stack = calloc(1, sizeof(struct IntegerStack));
+  STACK stack = calloc(1, sizeof(*stack));
   if (IS_NULL(stack)) { errno = ENOMEM; return NULL; }
 
-  stack->top = NULL, stack->size = 0;
+  IntStack_Init(stack);
   return stack;
 }
 
-void IntStack_Free(STACK *stack) {
+void IntStack_Init(STACK stack) {
+  if (IS_NULL(stack)) { return; }
+  stack->top = NULL;
+  stack->size = 0;
+}
+
+void IntStack_Clear(STACK stack) {
+  if (IS_NULL(stack)) { return; }
+
+  while (!IntStack_IsEmpty(stack)) {
+    IntStack_Pop(stack, NULL);
+  }
+}
+
+void IntStack_Delete(STACK *stack) {
   if (IS_NULL(stack) || IS_NULL(*stack)) { return; }
 
-  while (!IntStack_IsEmpty(*stack)) {
-    IntStack_Pop(*stack);
-  }
-
+  IntStack_Clear(*stack);
   free(*stack), (*stack = NULL);
 }
 
 int IntStack_Push(STACK stack, ITEM data) {
   if (IS_NULL(stack)) { errno = EINVAL; return -1; }
 
-  NODE node = calloc(1, sizeof(struct IntegerStackNode));
+  NODE node = calloc(1, sizeof(*node));
   if (IS_NULL(node)) { errno = ENOMEM; return -1; }
 
-  node->data = data;
+  node->item = data;
   node->next = stack->top;
   stack->top = node;
   stack->size++;
+
   return 0;
 }
 
-ITEM IntStack_Pop(STACK stack) {
+int IntStack_Pop(STACK stack, ITEM *out) {
   if (IS_NULL(stack)) { errno = EINVAL; return -1; }
-  if (IntStack_IsEmpty(stack)) { errno = ENODATA; return -1; }
+  if (IS_NULL(stack->top)) { errno = ENODATA; return -1; }
+
+  if (IS_NOT_NULL(out)) {
+    *out = stack->top->item;
+  }
 
   NODE next = stack->top->next;
-  ITEM data = stack->top->data;
   free(stack->top), (stack->top = NULL);
-
   stack->top = next;
   stack->size--;
-  return data;
+
+  return 0;
 }
 
-ITEM IntStack_Peek(STACK stack) {
+int IntStack_Peek(STACK stack, ITEM *out) {
   if (IS_NULL(stack)) { errno = EINVAL; return -1; }
-  if (IntStack_IsEmpty(stack)) { errno = ENODATA; return -1; }
+  if (IS_NULL(stack->top)) { errno = ENODATA; return -1; }
 
-  return stack->top->data;
+  if (IS_NOT_NULL(out)) {
+    *out = stack->top->item;
+  }
+  return 0;
 }
 
-inline size_t IntStack_Size(STACK stack) {
+inline int IntStack_Size(STACK stack) {
   return stack->size;
 }
 
@@ -80,45 +91,43 @@ inline bool IntStack_IsEmpty(STACK stack) {
   return IntStack_Size(stack) == 0;
 }
 
-
-struct IntegerStackIterator {
-  STACK stack;
-  NODE next;
-};
-
-#define ITER IntStackIter
-
 ITER IntStackIter_Create(STACK stack) {
   if (IS_NULL(stack)) { errno = EINVAL; return NULL; }
 
-  ITER iterator = calloc(1, sizeof(struct IntegerStackIterator));
+  ITER iterator = calloc(1, sizeof(*iterator));
   if (IS_NULL(iterator)) { errno = ENOMEM; return NULL; }
 
-  iterator->stack = stack;
-  iterator->next = stack->top;
+  IntStackIter_Init(iterator, stack);
   return iterator;
 }
 
-void IntStackIter_Free(ITER *iterator) {
-  if (IS_NULL(iterator) || IS_NULL(*iterator)) { return; }
+inline void IntStackIter_Init(ITER iterator, STACK stack) {
+  iterator->cur = stack->top;
+}
+
+inline void IntStackIter_Clear(ITER iterator) {
+  memset(iterator, 0, sizeof(*iterator));
+}
+
+inline void IntStackIter_Delete(ITER *iterator) {
+  IntStackIter_Clear(*iterator);
   free(*iterator), (*iterator = NULL);
 }
 
 inline bool IntStackIter_HasNext(ITER iterator) {
-  return IS_NULL(iterator->next) ? false : true;
+  return IS_NULL(iterator->cur) ? false : true;
 }
 
 int IntStackIter_GetNext(ITER iterator, ITEM *out) {
   if (IS_NULL(iterator)) { errno = EINVAL; return -1; }
-  if (!IntStackIter_HasNext(iterator)) { errno = ENODATA; return -1; }
+  if (IS_NULL(iterator->cur)) { errno = ENODATA; return -1; }
 
-  *out = iterator->next->data;
-  iterator->next = iterator->next->next;
+  *out = iterator->cur->item;
+  iterator->cur = iterator->cur->next;
   return 0;
 }
 
-#undef ITER
 #undef NODE
+#undef ITER
 #undef ITEM
 #undef STACK
-
--- lib/stack/stack_int/stack_int.h
+++ lib/stack/stack_int/stack_int.h
@@ -1,57 +1,51 @@
 #include <stdbool.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifndef __int_stack_h
 #define __int_stack_h
 
-/** Stack of integers (LIFO order) */
+struct IntegerStackNode;
 struct IntegerStack;
+struct IntegerStackIterator;
+
 typedef struct IntegerStack *IntStack;
 typedef int64_t IntStackItem;
+typedef struct IntegerStackIterator *IntStackIterator;
 
 #define STACK IntStack
 #define ITEM IntStackItem
+#define ITER IntStackIterator
+#define NODE struct IntegerStackNode *
 
-/** Creates a growable stack of integers (LIFO order) on heap. */
+struct IntegerStack {
+  NODE top;
+  size_t size;
+};
+
 STACK IntStack_Create();
- void IntStack_Free(STACK *stack);
+ void IntStack_Init(STACK stack);
+ void IntStack_Clear(STACK stack);
+ void IntStack_Delete(STACK *stack);
 
-/** Adds an integer to the stack. On success returns `0`, on failure
- * sets `errno` and returns `-1`. */
-int IntStack_Push(STACK stack, ITEM integer);
+  int IntStack_Push(STACK stack, ITEM item);
+  int IntStack_Pop(STACK stack, ITEM *out);
+  int IntStack_Peek(STACK stack, ITEM *out);
+  int IntStack_Size(STACK stack);
+ bool IntStack_IsEmpty(STACK stack);
 
-/** Deletes and returns the most recently added integer from the stack, on failure
- * sets `errno` and returns `-1`. Use `IntStack_Size` or `IntStack_IsEmpty` to
- * determine if stack contains items.
- */
-ITEM IntStack_Pop(STACK stack);
+struct IntegerStackIterator {
+  NODE cur;
+};
 
-/** Returns the most recently added item from the stack, on failure
- * sets `errno` and returns `-1`. */
-ITEM IntStack_Peek(STACK stack);
-
-size_t IntStack_Size(STACK stack);
-bool IntStack_IsEmpty(STACK stack);
-
-/** Iterator to iterate over the integers in `struct IntegerStack` instance in
- * LIFO order. */
-struct IntegerStackIterator;
-typedef struct IntegerStackIterator *IntStackIter;
-
-#define ITER IntStackIter
-
-/** Creates an Iterator that yields the integers in `struct IntegerStack` in
- * LIFO order. */
 ITER IntStackIter_Create(STACK stack);
-void IntStackIter_Free(ITER *iterator);
+void IntStackIter_Init(ITER iterator, STACK stack);
+void IntStackIter_Clear(ITER iterator);
+void IntStackIter_Delete(ITER *iterator);
 bool IntStackIter_HasNext(ITER iterator);
+ int IntStackIter_GetNext(ITER iterator, ITEM *out);
 
-/** If there is a next item to consume then it sets the out parameter `data` to
- * the next integer on stack and returns `0`, otherwise sets `errno` and returns
- * `-1`. */
-int IntStackIter_GetNext(ITER iterator, ITEM *data);
-
+#undef NODE
 #undef ITER
 #undef ITEM
 #undef STACK
