625c249f504b0de46d0ec249641fd05728dc8051
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2026-05-08 16:17:05 +0530

    Refactor stack_fixed_capacity client

--- clients/stack/stack_fixed_capacity_client.c
+++ clients/stack/stack_fixed_capacity_client.c
@@ -1,7 +1,7 @@
+#include "stack_fixed_capacity.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "stack_fixed_capacity.h"
 
 #define STACK_CAPACITY 8
 #define BUFFER_SIZE 60
@@ -11,19 +11,19 @@
  * % cmake -S . -B build
  * % make -C build
  *
- * ### Run:
+ * ### Run
  * % echo "I II III IV V - - x" > input.txt
  * % ./build/bin/stack_fixed_capacity_client < input.txt
  *
  */
 int main() {
-  struct FixedCapacityStack _stack, *stack = &_stack;
-  Stack_Init(stack, STACK_CAPACITY);
+  printf("** FixedCapacityStack of capacity %02d **\n", STACK_CAPACITY);
+  printf("=======================================\n");
+  printf("\n");
 
   char inputBuffer[BUFFER_SIZE];
-
-  printf("** Stack (Fixed Capacity of %d) **\n", STACK_CAPACITY);
-  printf("\n");
+  struct FixedCapacityStack _stack, *stack = &_stack;
+  Stack_Init(stack, STACK_CAPACITY);
 
   while (fscanf(stdin, "%59s", inputBuffer) != EOF) {
     if (strncmp(inputBuffer, "x\0", 2) == 0) {
@@ -31,40 +31,39 @@ int main() {
     }
 
     if (strncmp(inputBuffer, "-\0", 2) == 0) {
-      if (Stack_Size(stack) > 0) {
-        char *popped = (char *) Stack_Pop(stack);
-        printf("pop() = %s\n", popped);
-        free(popped);
-      } else {
+      if (Stack_IsEmpty(stack)) {
         printf("Stack is empty\n");
+        continue;
       }
-    }
-    else {
-      if (Stack_Size(stack) < STACK_CAPACITY) {
-        size_t bufLen = strlen(inputBuffer);
-        char *input = calloc(bufLen + 1, sizeof(char));
-        snprintf(input, bufLen + 1, "%s", inputBuffer);
-        Stack_Push(stack, input);
-        printf("push(%s)\n", input);
 
-        if (Stack_Size(stack) == STACK_CAPACITY) {
-          printf("Stack is full\n");
-        }
-      } else {
-        printf("Stack is full\n");
-      }
+      char *popped = (char *)Stack_Pop(stack);
+      printf("pop() = %s\n", popped);
+      free(popped), (popped = NULL);
+      continue;
     }
+
+    if (Stack_Size(stack) >= STACK_CAPACITY) {
+      printf("Stack is full\n");
+      continue;
+    }
+
+    size_t bufLen = strlen(inputBuffer);
+    char *input = calloc(bufLen + 1, sizeof(*input));
+    snprintf(input, bufLen + 1, "%s", inputBuffer);
+    Stack_Push(stack, input);
+    printf("push(%s)\n", input);
   }
 
   printf("\n");
-  printf("peek() = %s\n", (char *) Stack_Peek(stack));
+  printf("isEmpty() = %s\n", Stack_IsEmpty(stack) ? "true" : "false");
   printf("size() = %d\n", Stack_Size(stack));
+  printf("peek() = %s\n", (char *)Stack_Peek(stack));
 
   struct FCStackIterator _iterator, *iterator = &_iterator;
   StackIterator_Init(iterator, stack);
 
   while (StackIterator_HasNext(iterator)) {
-    char *input = (char *) StackIterator_GetNext(iterator);
+    char *input = (char *)StackIterator_GetNext(iterator);
     printf("iterator_next() = %s\n", input);
   }
   printf("\n");
@@ -72,7 +71,7 @@ int main() {
   StackIterator_Clear(iterator), (iterator = NULL);
 
   while (!Stack_IsEmpty(stack)) {
-    free((char *) Stack_Pop(stack));
+    free((char *)Stack_Pop(stack));
   }
   Stack_Clear(stack), (stack = NULL);
 
@@ -81,4 +80,3 @@ int main() {
 
 #undef STACK_CAPACITY
 #undef BUFFER_SIZE
-
