23d28349f24156d8c8cb0d6a8108de854ecc43dd
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2026-05-10 16:03:04 +0530

    Update test for stack_int

--- tests/stack/test_stack_int.c
+++ tests/stack/test_stack_int.c
@@ -1,147 +1,172 @@
-#include <criterion/criterion.h>
-#include <errno.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include "stack_int.h"
+#include <criterion/criterion.h>
 
 static struct IntegerStack _stack, *stack = &_stack;
 
 static void setup() {
-  IntStack_Init(stack);
-  cr_assert(stack != NULL);
+  const int code = IntStack_Init(stack);
+  cr_assert_eq(code, EXIT_SUCCESS);
 }
 
 static void teardown() {
-  IntStack_Clear(stack);
+  const int code = IntStack_Clear(stack);
+  cr_assert_eq(code, EXIT_SUCCESS);
 }
 
+
 Test(
-    int_stack_push,
-    pushes_data_onto_stack,
+    int_stack,
+    initializes_to_valid_state,
     .init = setup,
     .fini = teardown
 ) {
-  IntStack_Push(stack, -1);
-  IntStack_Push(stack,  0);
-  IntStack_Push(stack, 10);
-
-  cr_assert(IntStack_Size(stack) == 3);
-  cr_assert(IntStack_IsEmpty(stack) == false);
+  IntStackItem integer;
+  cr_assert_eq(IntStack_IsEmpty(stack), true);
+  cr_assert_eq(IntStack_Size(stack), 0);
+  cr_assert_neq(IntStack_Peek(stack, &integer), EXIT_SUCCESS);
+  cr_assert_neq(IntStack_Pop(stack, &integer), EXIT_SUCCESS);
 
   struct IntegerStackIterator _iterator, *iterator = &_iterator;
   IntStackIter_Init(iterator, stack);
-
-  IntStackItem integer;
-  cr_assert(IntStackIter_HasNext(iterator) == true);
-  cr_assert(IntStackIter_GetNext(iterator, &integer) == EXIT_SUCCESS);
-  cr_assert(10 == integer);
-
-  cr_assert(IntStackIter_HasNext(iterator) == true);
-  cr_assert(IntStackIter_GetNext(iterator, &integer) == EXIT_SUCCESS);
-  cr_assert(0 == integer);
-
-  cr_assert(IntStackIter_HasNext(iterator) == true);
-  cr_assert(IntStackIter_GetNext(iterator, &integer) == EXIT_SUCCESS);
-  cr_assert(-1 == integer);
-
-  cr_assert(IntStackIter_HasNext(iterator) == false);
-  cr_assert(IntStackIter_GetNext(iterator, &integer) == EXIT_FAILURE);
-
-  IntStackIter_Clear(iterator);
+  cr_assert_eq(IntStackIter_HasNext(iterator), false);
+  cr_assert_eq(IntStackIter_GetNext(iterator, &integer), EXIT_FAILURE);
+  IntStackIter_Clear(iterator), (iterator = NULL);
 }
 
 
 Test(
-    int_stack_pop,
-    pops_data_from_stack,
+    int_stack,
+    allows_pushing_arbitrary_number_of_items,
     .init = setup,
     .fini = teardown
 ) {
-  IntStack_Push(stack, -1);
-  IntStack_Push(stack,  0);
-  IntStack_Push(stack, 10);
-
-  IntStackItem integer;
-  cr_assert(IntStack_IsEmpty(stack) == false);
-  cr_assert(IntStack_Pop(stack, &integer) == EXIT_SUCCESS);
-  cr_assert(integer == 10);
-
-  cr_assert(IntStack_IsEmpty(stack) == false);
-  cr_assert(IntStack_Pop(stack, &integer) == EXIT_SUCCESS);
-  cr_assert(integer == 0);
-
-  cr_assert(IntStack_IsEmpty(stack) == false);
-  cr_assert(IntStack_Pop(stack, &integer) == EXIT_SUCCESS);
-  cr_assert(integer == -1);
-
-  cr_assert(IntStack_IsEmpty(stack) == true);
-  cr_assert(IntStack_Pop(stack, &integer) == EXIT_FAILURE);
-}
-
-
-Test(
-    int_stack_push,
-    exhaustive_pushes_onto_stack,
-    .init = setup,
-    .fini = teardown
-) {
-  const int integersSize = INT16_MAX;
-  IntStackItem integers[integersSize];
-
-  for (int i = 0; i < integersSize; i++) {
-    integers[i] = i;
-    cr_assert(IntStack_Push(stack, i) == EXIT_SUCCESS);
+  const int n = 1000256;
+  IntStackItem arr[n];
+  for (int i = 0; i < n; i++) {
+    arr[i] = i;
+    cr_expect_eq(IntStack_Push(stack, arr[i]), EXIT_SUCCESS);
   }
-  cr_assert(IntStack_Size(stack) == integersSize);
 
-  int i = 0;
+  const int expected = n;
+  const int actual = IntStack_Size(stack);
+  cr_expect_eq(actual, expected, "Expected stack size to be %d, got %d",
+               expected, actual);
+}
+
+
+Test(
+    int_stack,
+    updates_state_correctly_after_every_push,
+    .init = setup,
+    .fini = teardown
+) {
+  const int n = 10;
+  const IntStackItem arr[n] = {11, 20, 33, 44, 55, 66, 77, 88, 99, 110};
+
+  IntStackItem integer;
+
+  for (int i = 0; i < n; i++) {
+    cr_assert_eq(IntStack_Push(stack, arr[i]), EXIT_SUCCESS);
+    cr_expect_eq(IntStack_IsEmpty(stack), false);
+    cr_expect_eq(IntStack_Size(stack), i + 1);
+    cr_expect_eq(IntStack_Peek(stack, &integer), EXIT_SUCCESS);
+    cr_expect_eq(integer, arr[i]);
+  }
+}
+
+
+Test(
+    int_stack,
+    pushes_items_in_correct_order,
+    .init = setup,
+    .fini = teardown
+) {
+  const int n = 10;
+  const IntStackItem arr[n] = {11, 20, 33, 44, 55, 66, 77, 88, 99, 110};
+  for (int i = 0; i < n; i++) {
+    cr_assert_eq(IntStack_Push(stack, arr[i]), EXIT_SUCCESS);
+  }
+
+  int i;
+  IntStackItem actual, expected;
   struct IntegerStackIterator _iterator, *iterator = &_iterator;
-  IntStackIter_Init(iterator, stack);
 
-  cr_assert(IntStackIter_HasNext(iterator) == true);
+  cr_expect_eq(IntStackIter_Init(iterator, stack), EXIT_SUCCESS);
+  cr_expect_eq(IntStackIter_HasNext(iterator), true);
 
-  for (i = 0; IntStackIter_HasNext(iterator); i++) {
-    IntStackItem actual = 0;
-    cr_assert(IntStackIter_GetNext(iterator, &actual) == EXIT_SUCCESS);
-
-    const IntStackItem expected = integers[(integersSize - 1) - i];
-    cr_assert(actual == expected,
-              "Expected next integer to be %lld, it is %lld", expected,
-              actual);
+  // Items should be in reverse order
+  for (i = n - 1; IntStackIter_HasNext(iterator); i--) {
+    cr_assert(i >= 0);
+    expected = arr[i];
+    cr_expect_eq(IntStackIter_GetNext(iterator, &actual), EXIT_SUCCESS);
+    cr_expect_eq(actual, expected, "Expected next integer to be %lld, got %lld",
+                 expected, actual);
   }
+  cr_expect_eq(i, -1); // Assert that all the items are consumed
 
-  cr_assert(i == integersSize, "Expected the iterator to consume all %d items, "
-                               "but %d were consumed", integersSize, i);
-
-  IntStackIter_Clear(iterator);
+  cr_expect_eq(IntStackIter_Clear(iterator), EXIT_SUCCESS);
 }
 
 
 Test(
-    int_stack_pop,
-    exhaustive_pops_from_stack,
+    int_stack,
+    updates_state_correctly_after_every_pop,
     .init = setup,
     .fini = teardown
 ) {
-  const int integersSize = INT16_MAX;
-  IntStackItem integers[integersSize];
-
-  for (int i = 0; i < integersSize; i++) {
-    integers[i] = i;
-    cr_assert(IntStack_Push(stack, i) == EXIT_SUCCESS);
+  const int n = 10;
+  const IntStackItem arr[n] = {11, 20, 33, 44, 55, 66, 77, 88, 99, 110};
+  for (int i = 0; i < n; i++) {
+    cr_assert_eq(IntStack_Push(stack, arr[i]), EXIT_SUCCESS);
   }
 
-  cr_assert(IntStack_Size(stack) == integersSize);
+  IntStackItem integer;
 
-  IntStackItem i, actual;
+  for (int i = 0; i < n; i++) {
+    cr_assert_eq(IntStack_Pop(stack, &integer), EXIT_SUCCESS);
 
-  for (i = 0; !IntStack_IsEmpty(stack); i++) {
-    const IntStackItem expected = integers[(integersSize - 1) - i];
-    cr_assert(IntStack_Pop(stack, &actual) == EXIT_SUCCESS);
-    cr_assert(actual == expected);
+    const bool expectedEmpty = i == n - 1;
+    const int expectedStackSize = n - 1 - i;
+
+    cr_expect_eq(IntStack_IsEmpty(stack), expectedEmpty);
+    cr_expect_eq(IntStack_Size(stack), expectedStackSize);
+
+    if (expectedEmpty) {
+      cr_expect_eq(IntStack_Peek(stack, &integer), EXIT_FAILURE);
+    } else {
+      const IntStackItem expectedPeekedItem = arr[n - 2 - i];
+      cr_expect_eq(IntStack_Peek(stack, &integer), EXIT_SUCCESS);
+      cr_expect_eq(integer, expectedPeekedItem);
+    }
   }
 
-  cr_assert(i == integersSize, "Expected the stack to pop all %d items, "
-                               "but only %lld were popped", integersSize, i);
+  struct IntegerStackIterator _iterator, *iterator = &_iterator;
+  cr_expect_eq(IntStackIter_Init(iterator, stack), EXIT_SUCCESS);
+  cr_expect_eq(IntStackIter_HasNext(iterator), false);
+  cr_expect_neq(IntStackIter_GetNext(iterator, &integer), EXIT_SUCCESS);
+  cr_expect_eq(IntStackIter_Clear(iterator), EXIT_SUCCESS);
+}
+
+
+Test(
+    int_stack,
+    pops_items_in_correct_order,
+    .init = setup,
+    .fini = teardown
+) {
+  const int n = 10;
+  const IntStackItem arr[n] = {11, 20, 33, 44, 55, 66, 77, 88, 99, 110};
+  for (int i = 0; i < n; i++) {
+    cr_assert_eq(IntStack_Push(stack, arr[i]), EXIT_SUCCESS);
+  }
+
+  IntStackItem actual, expected;
+
+  for (int i = n - 1; i >= 0; i--) {
+    expected = arr[i];
+    cr_expect_eq(IntStack_Pop(stack, &actual), EXIT_SUCCESS);
+    cr_expect_eq(actual, expected, "Expected popped integer to be %lld, got %lld",
+              expected, actual);
+  }
 }
 
