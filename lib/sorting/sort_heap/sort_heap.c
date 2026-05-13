1b62401da4ee2572009f0c1e134c399d7fe566eb
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2026-05-13 22:03:37 +0530

    Refactor heap sort

--- lib/sorting/sort_heap/sort_heap.c
+++ lib/sorting/sort_heap/sort_heap.c
@@ -1,16 +1,15 @@
 #include <assert.h>
+#include <errno.h>
 #include <memory.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include "sort_heap.h"
+
 #include "arr_utils.h"
+#include "common_macros.h"
+#include "sort_heap.h"
 
 void sinkDown(void *arr, size_t width, int n, int p, ComparatorFn cmp) {
-  for (
-      int c = 2 * p + 1;
-      c < n;
-      p = c, c = 2 * p + 1
-  ) {
+  for (int c = 2 * p + 1; c < n; p = c, c = 2 * p + 1) {
     if (c + 1 < n && cmp(arr + c * width, arr + (c + 1) * width) < 0) {
       c += 1;
     }
@@ -22,16 +21,21 @@ void sinkDown(void *arr, size_t width, int n, int p, ComparatorFn cmp) {
   }
 }
 
-void heapSort(void *arr, size_t width, int n, ComparatorFn cmp) {
+int Sort_Heap(void *arr, size_t width, int n, ComparatorFn cmp) {
+  REQUIRE_TRUE(IS_NOT_NULL(arr), EINVAL, EXIT_FAILURE);
+  REQUIRE_TRUE(width > 0, EINVAL, EXIT_FAILURE);
+  REQUIRE_TRUE(n > 0, EINVAL, EXIT_FAILURE);
+  REQUIRE_TRUE(IS_NOT_NULL(cmp), EINVAL, EXIT_FAILURE);
+
   for (int p = (n - 1 - 1) / 2; p >= 0; p--) {
     sinkDown(arr, width, n, p, cmp);
   }
 
-  for (int s = n; s > 0; ) {
+  for (int s = n; s > 0;) {
     ArrUtils_Swap(arr, width, 0, --s);
     sinkDown(arr, width, s, 0, cmp);
   }
 
   assert(ArrUtils_IsSorted(arr, width, n, cmp) == true);
+  return EXIT_SUCCESS;
 }
-
--- lib/sorting/sort_heap/sort_heap.h
+++ lib/sorting/sort_heap/sort_heap.h
@@ -1,10 +1,10 @@
 #include <stddef.h>
+
 #include "comparators.h"
 
 #ifndef __heap_sort_h
 #define __heap_sort_h
 
-void heapSort(void *arr, size_t width, int n, ComparatorFn cmp);
+int Sort_Heap(void *arr, size_t width, int n, ComparatorFn cmp);
 
 #endif // __heap_sort_h
-
