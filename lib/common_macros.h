7f7fb88dc763a579bc279e0a96b716ee03f5671e
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2026-05-22 14:24:21 +0530

    Update & reformat common_macros.c

--- lib/common_macros.h
+++ lib/common_macros.h
@@ -1,29 +1,34 @@
+#include <errno.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifndef __common_macros_h
 #define __common_macros_h
 
 #define IS_NULL(ptr) (NULL == ptr)
 #define IS_NOT_NULL(ptr) (NULL != ptr)
-#define BOOL_TO_STRING(val) (val == true ? "true" : val == false ? "false" : "<invalid>")
+#define BOOL_TO_STRING(val)                                                    \
+  (val == true ? "true" : val == false ? "false" : "<invalid>")
 
-#define ENSURE_CALL_SUCCESS(_callExpr) \
-  do {                     \
-    int code = _callExpr;  \
-    if (code != 0) {       \
-      fprintf(stderr, "Call %s failed at %s:%d\n\n", #_callExpr, __FILE__, __LINE__); \
-      return code;         \
-    }                      \
-  } while (0)              \
+#define ENSURE_CALL_SUCCESS(_callExpr)                                         \
+  do {                                                                         \
+    int code = _callExpr;                                                      \
+    if (code != EXIT_SUCCESS) {                                                \
+      fprintf(stderr, "Error: %s\n", strerror(errno));                         \
+      fprintf(stderr, "Call %s at %s:%d\n", #_callExpr, __FILE__, __LINE__);   \
+      fprintf(stderr, "\n");                                                   \
+      return code;                                                             \
+    }                                                                          \
+  } while (0)
 
-#define REQUIRE_TRUE(_expr, _errorCode, _returnVal) \
-  do {                     \
-    bool isTrue = _expr;   \
-    if (!isTrue) {         \
-      errno = _errorCode;  \
-      return _returnVal;  \
-    }                      \
-  } while (0)              \
+#define REQUIRE_TRUE(_expr, _errorCode, _returnVal)                            \
+  do {                                                                         \
+    bool isTrue = _expr;                                                       \
+    if (!isTrue) {                                                             \
+      errno = _errorCode;                                                      \
+      return _returnVal;                                                       \
+    }                                                                          \
+  } while (0)
 
 #endif // __common_macros_h
-
